27e06bd40ba06f64149dc8d29d2d74cba7a1fe09
mitchel <mitchel.veerdonk@outlook.com>
2017-04-27 22:10:53 +0200

    codecademy scripts

---
+++ datetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+now = datetime.now()  ## Prints current date and time
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+
+## Code runs on codecademy, but not on python itself? What's wrong here??
+
